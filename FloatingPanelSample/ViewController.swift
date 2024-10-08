5ed980c98edf061849b9c281fa21ab820858a5ea
masahito-mori <mmori.0426@gmail.com>
2024-10-08 15:26:44 +0900

    ⚙️プロジェクトの初期化
    【対応内容】
    ・GUIファイルを削除
    ・SPMでfloatingPanelを追加
    
    【影響範囲】
    ・アプリ全体
    
    【動作確認】
    ・ビルドに成功することを確認

---
+++ FloatingPanelSample/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FloatingPanelSample
+//
+//  Created by Masahito Mori on 2024/10/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+}
+
